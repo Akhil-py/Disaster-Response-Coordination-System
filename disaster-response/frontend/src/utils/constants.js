f5228b63f7aa00adb6cc8979b842c683532106c3
Akhil Ram Shankar <ramshankar.akhil@gmail.com>
2026-02-17 09:41:00 -0800

    feat(utils): constants.js MESSAGE_TYPES SEVERITY_COLORS RESOURCE_ICONS

---
+++ disaster-response/frontend/src/utils/constants.js
@@ -0,0 +1,21 @@
+export const MESSAGE_TYPES = {
+  STATE_SYNC: 'state_sync',
+  ACTIVITY: 'activity',
+  ERROR: 'error',
+  ASSIGN: 'assign',
+  PING: 'ping',
+};
+
+export const SEVERITY_COLORS = {
+  0: '#1a2a1a',
+  1: '#2a3a1a',
+  2: '#3a2a0a',
+  3: '#3a1a0a',
+  4: '#4a0a0a',
+};
+
+export const RESOURCE_ICONS = {
+  ambulance: '✚',
+  fire_truck: '🔥',
+  shelter: '⌂',
+};
